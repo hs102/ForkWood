2f6e8b01afa411d17a8041df63a84ab31e8f4367
Omar Assadi <Omarassadi825@gmail.com>
2024-04-01 00:14:15 +0300

    Adding the firebase integration #8

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,30 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { provideFirestore, getFirestore } from '@angular/fire/firestore';
+
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB3ek2lFK9JwTtru3pUZk7JfMEQAjPiMYs",
+  authDomain: "worood-db.firebaseapp.com",
+  projectId: "worood-db",
+  storageBucket: "worood-db.appspot.com",
+  messagingSenderId: "441607635986",
+  appId: "1:441607635986:web:8b909578eea0579de6c4cb"
+};
+
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule , 
+  // initialize angularfire with credentials from the dashboard
+  provideFirebaseApp(() => initializeApp(firebaseConfig)),
+  // Import the AngularFireDatabaseModule to use database
+  provideFirestore(() => getFirestore()),
+  ],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
+  
 })
 export class AppModule {}
