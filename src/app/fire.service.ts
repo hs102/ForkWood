f453ebd7e04593caca28b244cd849957bba79d45
Omar Eldanasoury <omardanasoury8@gmail.com>
2024-04-01 15:23:18 +0300

    Added firebase's firestore libraries to FireService #8

--- src/app/fire.service.ts
+++ src/app/fire.service.ts
@@ -1,5 +1,12 @@
 import { Injectable } from '@angular/core';
 
+import { Firestore } from '@angular/fire/firestore';
+import { collection, collectionData, CollectionReference, DocumentReference } from '@angular/fire/firestore';
+import { getDocs, doc, deleteDoc, updateDoc, docData, setDoc, addDoc, query } from '@angular/fire/firestore';
+import { DocumentData } from 'firebase/firestore';
+import { Observable } from 'rxjs';
+
+
 import {
     Auth,
     getAuth,
@@ -15,5 +22,8 @@ import {
 })
 export class FireService {
 
-  constructor(public auth : Auth) { }
+  constructor(public firestore : Firestore,
+              public auth : Auth,
+              
+              ) { }
 }
